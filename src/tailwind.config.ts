fc27fb90084a06413ba8cf62f5427dc7bf8da4ec
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2022-05-02 20:10:59 -0300

    fix: fix watching issues when using Nuxt Bridge

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -10,10 +10,14 @@ export default ({ srcDir }) => ({
     `${srcDir}/pages/**/*.vue`,
     `${srcDir}/composables/**/*.{js,ts}`,
     `${srcDir}/plugins/**/*.{js,ts}`,
-    `${srcDir}/App.{js,ts,vue}`,
-    `${srcDir}/app.{js,ts,vue}`
     // TODO: This makes issues with import protection
     // Split in two files to avoid watching issues (https://github.com/nuxt-community/tailwindcss-module/issues/359)
+    `${srcDir}/App.js`,
+    `${srcDir}/App.ts`,
+    `${srcDir}/App.vue`,
+    `${srcDir}/app.js`,
+    `${srcDir}/app.ts`,
+    `${srcDir}/app.vue`
     // `${rootDir}/nuxt.config.js`,
     // `${rootDir}/nuxt.config.ts`
   ]
